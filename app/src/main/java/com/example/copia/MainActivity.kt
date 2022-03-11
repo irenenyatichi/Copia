5e12d9b29bf7da824a3bab336b44c96fb8e920f2
irenenyatichi <irenenyatichik@gmail.com>
2022-03-11 10:04:29 +0300

    Listing Payments & Receipts

--- app/src/main/java/com/example/copia/MainActivity.kt
+++ app/src/main/java/com/example/copia/MainActivity.kt
@@ -8,6 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        fun payments(MG001: Int, MG002: Int, MG003: Int, MG004: Int) {
+            val transactionRef = payments(100, 200,300,250)
+        }
+
+        fun receiptsTable(R001: Int, R002: Int, R003: Int) {
+            val amountsToBePaid = receiptsTable(100, 400, 350)
+        }
 
     }
+}
