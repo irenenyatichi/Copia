a1a0330e31e4613693a423126b07d6b402fb229c
irenenyatichi <irenenyatichik@gmail.com>
2022-03-11 11:41:10 +0300

    Iteration

--- app/src/main/java/com/example/copia/MainActivity.kt
+++ app/src/main/java/com/example/copia/MainActivity.kt
@@ -9,13 +9,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun payments(MG001: Int, MG002: Int, MG003: Int, MG004: Int) {
-            val transactionRef = payments(100, 200,300,250)
-        }
+        fun payments(MG001: Int, MG002: Int, MG003: Int, MG004: Int):String {
+            val transactionRef = mutableListOf(100, 200, 300, 250)
 
-        fun receiptsTable(R001: Int, R002: Int, R003: Int) {
-            val amountsToBePaid = receiptsTable(100, 400, 350)
-        }
 
+        fun receiptsTable(R001: Int, R002: Int, R003: Int):Int {
+            val amountsToBePaid = receiptsTable(100,400,350)
+
+            val result = when (amountsToBePaid) {
+                100 -> MG001
+                200 -> MG002
+                300 -> MG003
+                250 -> MG004
+
+                else -> { val difference = amountsToBePaid-transactionRef
+                when (difference){
+                    100 -> MG001
+                    200 -> MG002
+                    300 -> MG003
+                    250 -> MG004
+
+                    else -> "Not Found"
+                }
+                    }                    }
+            println(result)
+            return receiptsTable(100,400,350)
+        }
+            return payments(100, 200, 300, 250)
+        }
     }
+}
+
+private operator fun Int.minus(transactionRef: MutableList<Int>):Int {
+    return Int.MAX_VALUE
 }
